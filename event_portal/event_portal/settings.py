f452acb6a433a3c726924e6656f48418651ca94d
Astha Pardeshi <asthapardeshi354@gmail.com>
2026-08-09 15:45:13 +0530

    Fix PostgreSQL database configuration

--- event_portal/event_portal/settings.py
+++ event_portal/event_portal/settings.py
@@ -1,3 +1,4 @@
+
 """
 Django settings for event_portal project.
 
@@ -11,6 +12,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,10 +79,10 @@ WSGI_APPLICATION = "event_portal.wsgi.application"
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+    )
 }
 
 
